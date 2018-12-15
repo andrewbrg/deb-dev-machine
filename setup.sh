9f5171de9ad409f6a4c0419da212c8f1fca347f5
Andrew Borg <andrewbrg87@gmail.com>
2018-12-15 21:46:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Disallow running with sudo or su
 ##########################################################
-if [ "$EUID" -eq 0 ]
+if [[ "$EUID" -eq 0 ]]
   then printf "\033[1;101mNein, Nein, Nein!! Please do not run this script as root (no su or sudo)! \033[0m \n";
   exit;
 fi
@@ -51,7 +51,7 @@ gotGoLang=0;
 # PHP 7.3
 ##########################################################
 repoPhp() {
-    if [ ! -f /etc/apt/sources.list.d/php.list ]; then
+    if [[ ! -f /etc/apt/sources.list.d/php.list ]]; then
         notify "Adding PHP sury repository";
         curl -fsSL "https://packages.sury.org/php/apt.gpg" | sudo apt-key add -;
         echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list;
@@ -61,7 +61,7 @@ repoPhp() {
 # Yarn
 ##########################################################
 repoYarn() {
-    if [ ! -f /etc/apt/sources.list.d/yarn.list ]; then
+    if [[ ! -f /etc/apt/sources.list.d/yarn.list ]]; then
         notify "Adding Yarn repository";
         curl -fsSL "https://dl.yarnpkg.com/debian/pubkey.gpg" | sudo apt-key add -;
         echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list;
@@ -71,29 +71,29 @@ repoYarn() {
 # Docker CE
 ##########################################################
 repoDocker() {
-    if [ ! -f /var/lib/dpkg/info/docker-ce.list ]; then
+    if [[ ! -f /var/lib/dpkg/info/docker-ce.list ]]; then
         notify "Adding Docker repository";
         curl -fsSL "https://download.docker.com/linux/debian/gpg" | sudo apt-key add -;
         sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable";
     sudo groupadd docker;
-    sudo usermod -aG docker $USER;
+    sudo usermod -aG docker ${USER};
     fi
 }
 
 # Kubernetes
 ##########################################################
 repoKubernetes() {
-    if [ ! -f /etc/apt/sources.list.d/kubernetes.list ]; then
+    if [[ ! -f /etc/apt/sources.list.d/kubernetes.list ]]; then
         notify "Adding Kubernetes repository";
         curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo apt-key add -;
         echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list;
-    fi   
+    fi
 }
 
 # Wine
 ##########################################################
 repoWine() {
-    if [ ! -f /var/lib/dpkg/info/wine-stable.list ]; then
+    if [[ ! -f /var/lib/dpkg/info/wine-stable.list ]]; then
         notify "Adding Wine repository";
         sudo dpkg --add-architecture i386;
         curl -fsSL "https://dl.winehq.org/wine-builds/Release.key" | sudo apt-key add -;
@@ -104,7 +104,7 @@ repoWine() {
 # Atom
 ##########################################################
 repoAtom() {
-    if [ ! -f /etc/apt/sources.list.d/atom.list ]; then
+    if [[ ! -f /etc/apt/sources.list.d/atom.list ]]; then
         notify "Adding Atom IDE repository";
         curl -fsSL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -;
         echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" | sudo tee /etc/apt/sources.list.d/atom.list;
@@ -114,7 +114,7 @@ repoAtom() {
 # VS Code
 ##########################################################
 repoVsCode() {
-    if [ ! -f /etc/apt/sources.list.d/vscode.list ]; then
+    if [[ ! -f /etc/apt/sources.list.d/vscode.list ]]; then
         notify "Adding VS Code repository";
         curl "https://packages.microsoft.com/keys/microsoft.asc" | gpg --dearmor > microsoft.gpg;
         sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/;
@@ -125,7 +125,7 @@ repoVsCode() {
 # Sublime
 ##########################################################
 repoSublime() {
-    if [ ! -f /etc/apt/sources.list.d/sublime-text.list ]; then
+    if [[ ! -f /etc/apt/sources.list.d/sublime-text.list ]]; then
         notify "Adding Sublime text repository";
         curl -fsSL "https://download.sublimetext.com/sublimehq-pub.gpg" | sudo apt-key add -;
         echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list;
@@ -135,7 +135,7 @@ repoSublime() {
 # Remmina
 ##########################################################
 repoRemmina() {
-    if [ ! -f /etc/apt/sources.list.d/remmina.list ]; then
+    if [[ ! -f /etc/apt/sources.list.d/remmina.list ]]; then
         notify "Adding Remmina repository";
         sudo touch /etc/apt/sources.list.d/remmina.list;
         echo "deb http://ftp.debian.org/debian stretch-backports main" | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
@@ -145,7 +145,7 @@ repoRemmina() {
 # Google Cloud SDK
 ##########################################################
 repoGoogleSdk() {
-    if [ ! -f /etc/apt/sources.list.d/google-cloud-sdk.list ]; then
+    if [[ ! -f /etc/apt/sources.list.d/google-cloud-sdk.list ]]; then
         notify "Adding GCE repository";
         export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)";
         echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list;
@@ -156,7 +156,7 @@ repoGoogleSdk() {
 # VLC
 ##########################################################
 repoVlc() {
-    if [ ! -f /etc/apt/sources.list.d/videolan-ubuntu-stable-daily-disco.list ]; then
+    if [[ ! -f /etc/apt/sources.list.d/videolan-ubuntu-stable-daily-disco.list ]]; then
         notify "Adding VLC repository";
         sudo add-apt-repository ppa:videolan/stable-daily
     fi
@@ -230,7 +230,7 @@ installPhp() {
     sudo apt install -y php7.3 php7.3-{bcmath,cli,common,curl,dev,gd,intl,mbstring,mysql,sqlite3,xml,zip} php-pear php-memcached php-redis;
     sudo apt install -y libphp-predis php-xdebug php-ds;
     php --version;
-    
+
     sudo pecl install igbinary ds;
     gotPhp=1;
     breakLine;
@@ -262,7 +262,7 @@ installGoLang() {
     sudo apt install -y golang;
     echo 'export GOPATH=~/go' >> ~/.bashrc;
     source ~/.bashrc;
-    mkdir $GOPATH;
+    mkdir ${GOPATH};
     gotGoLang=1;
     breakLine;
 }
@@ -319,7 +319,7 @@ installLaravel() {
 installSqLite() {
     title "Installing SQLite Browser";
     sudo apt install -y sqlitebrowser;
-    breakLine;    
+    breakLine;
 }
 
 # DBeaver
@@ -333,7 +333,7 @@ installDbeaver() {
     libutempter0* \
     openjdk-8-jre-headless* \
     xbitmaps*;
-    
+
     curlToFile "https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb" "dbeaver.deb";
     sudo dpkg -i ~/dbeaver.deb;
     sudo rm ~/dbeaver.deb;
@@ -391,7 +391,7 @@ installSops() {
 ##########################################################
 installWine() {
     title "Installing Wine & Mono";
-    
+
     sudo apt install -y cabextract;
     sudo apt install -y --install-recommends winehq-stable;
     sudo apt install -y mono-vbnc;
@@ -401,17 +401,17 @@ installWine() {
     sudo chmod +x ~/winetricks;
     ./winetricks allfonts;
     echo "y" | rm ~/winetricks;
-    
+
     notify "Applying font smoothing to wine apps";
     curlToFile ${repoUrl}"wine_fontsmoothing.sh" "wine_fontsmoothing";
     sudo chmod +x ~/wine_fontsmoothing;
     sudo ./wine_fontsmoothing;
     echo "y" | rm ~/wine_fontsmoothing;
     clear;
-    
+
     notify "Installing Royale 2007 theme for windows apps";
     curlToFile "http://www.gratos.be/wincustomize/compressed/Royale_2007_for_XP_by_Baal_wa_astarte.zip" "Royale_2007.zip";
-    
+
     sudo chown -R $(whoami) ~/;
     mkdir -p ~/.wine/drive_c/Resources/Themes/;
     unzip ~/Royale_2007.zip -d ~/.wine/drive_c/Resources/Themes/;
@@ -425,14 +425,14 @@ installPostman() {
     title "Installing Postman";
     curlToFile "https://dl.pstmn.io/download/latest/linux64" "postman.tar.gz";
     sudo tar xfz ~/postman.tar.gz;
-    
+
     sudo rm -rf /opt/postman/;
     sudo mkdir /opt/postman/;
     sudo mv ~/Postman*/* /opt/postman/;
     sudo rm -rf ~/Postman*;
     sudo rm -rf ~/postman.tar.gz;
     sudo ln -s /opt/postman/Postman /usr/bin/postman;
-    
+
     notify "Adding desktop file for Postman";
     curlToFile ${repoUrl}"desktop/postman.desktop" "/usr/share/applications/postman.desktop";
     breakLine;
@@ -462,23 +462,23 @@ installSublime() {
     sudo apt install -y sublime-text;
     sudo apt install -y python-pip;
     sudo pip install -U CodeIntel;
-    
+
     sudo chown -R $(whoami) ~/;
-    
+
     mkdir -p ~/.config/sublime-text-3/Packages/User/;
-    
+
     notify "Adding package control for sublime";
     wget "https://packagecontrol.io/Package%20Control.sublime-package" -o ".config/sublime-text-3/Installed Packages/Package Control.sublime-package";
-    
+
     notify "Adding pre-installed packages for sublime";
     curlToFile "${repoUrl}settings/PackageControl.sublime-settings" ".config/sublime-text-3/Packages/User/Package Control.sublime-settings";
-    
+
     notify "Applying default preferences to sublime";
     curlToFile "${repoUrl}settings/Preferences.sublime-settings" ".config/sublime-text-3/Packages/User/Preferences.sublime-settings";
-    
+
     notify "Installing additional binaries for sublime auto-complete";
     curlToFile "https://github.com/emmetio/pyv8-binaries/raw/master/pyv8-linux64-p3.zip" "bin.zip";
-    
+
     sudo mkdir -p ".config/sublime-text-3/Installed Packages/PyV8/";
     sudo unzip ~/bin.zip -d ".config/sublime-text-3/Installed Packages/PyV8/";
     sudo rm ~/bin.zip;
@@ -491,13 +491,13 @@ installPhpStorm() {
     title "Installing PhpStorm IDE";
     curlToFile "https://download.jetbrains.com/webide/PhpStorm-2018.2.2.tar.gz" "phpstorm.tar.gz";
     sudo tar xfz ~/phpstorm.tar.gz;
-    
+
     sudo rm -rf /opt/phpstorm/;
     sudo mkdir /opt/phpstorm/;
     sudo mv ~/PhpStorm-*/* /opt/phpstorm/;
     sudo rm -rf ~/phpstorm.tar.gz;
     sudo rm -rf ~/PhpStorm-*;
-    
+
     notify "Adding desktop file for PhpStorm";
     curlToFile ${repoUrl}"desktop/jetbrains-phpstorm.desktop" "/usr/share/applications/jetbrains-phpstorm.desktop";
     breakLine;
@@ -524,20 +524,41 @@ installGoogleSdk() {
 installPopcorn() {
     title "Installing Popcorn Time";
     sudo apt install -y libnss3 vlc;
-    
-    if [ -d /opt/popcorn-time ]; then
+
+    if [[ -d /opt/popcorn-time ]]; then
         sudo rm -rf /opt/popcorn-time/
     fi
-    
+
     sudo mkdir /opt/popcorn-time;
     sudo wget -qO- "https://get.popcorntime.sh/build/Popcorn-Time-0.3.10-Linux-64.tar.xz" | sudo tar Jx -C /opt/popcorn-time;
     sudo ln -sf /opt/popcorn-time/Popcorn-Time /usr/bin/popcorn-time;
-     
+
     curlToFile ${repoUrl}"desktop/popcorn.desktop" "/usr/share/applications/popcorn.desktop";
-   
+
     breakLine;
 }
 
+# ZSH
+##########################################################
+installZsh() {
+    title "Installing ZSH Terminal Plugin";
+
+    sudo apt install -y zsh fonts-powerline;
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+    curlToFile ${repoUrl}"zsh/.zshrc" ".zshrc";
+
+    echo 'export GOPATH=~/go' >> ~/.zshrc;
+    source ~/.zshrc;
+
+    notify "Important - Access your terminal settings ('Ctrl-Shift-P' in ChromeOS), then"
+    echo "1) Set user-css path to: https://cdn.rawgit.com/wernight/powerline-web-fonts/e4d967ca4f95d9fa0cf1d51afed2e5a5927d759e/PowerlineFonts.css";
+    echo "2) Set font to: DejaVu Sans Mono";
+    echo "";
+    echo "Full docs: https://github.com/robbyrussell/oh-my-zsh";
+    sleep 2;
+
+    breakLine;
+}
 
 ###############################################################
 ## MAIN PROGRAM
@@ -549,7 +570,7 @@ cmd=(dialog --backtitle "Debian 9 Developer Container - USAGE: <space> select/un
 --clear \
 --nocancel \
 --separate-output \
---checklist "Select what you would like installed:" 39 50 50);
+--checklist "Select installable packages:" 40 50 50);
 
 options=(
     01 "Git" on
@@ -584,13 +605,14 @@ options=(
     30 "Remmina (Remote Desktop Client)" off
     31 "Google Cloud SDK" off
     32 "Popcorn Time" off
+    33 "ZSH Terminal Plugin" on
 );
 
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty);
 
 clear;
 
-# Preperation
+# Preparation
 ##########################################################
 title "Installing Pre-Requisite Packages";
     cd ~/;
@@ -598,7 +620,7 @@ title "Installing Pre-Requisite Packages";
     sudo apt update;
     sudo apt dist-upgrade -y;
     sudo apt autoremove -y --purge;
-    
+
     sudo apt install -y ca-certificates \
     apt-transport-https \
     software-properties-common \
@@ -615,14 +637,14 @@ title "Installing Pre-Requisite Packages";
     preload \
     gksu \
     snapd;
-    
+
     sudo updatedb;
 breakLine;
 
 title "Adding Repositories";
-for choice in $choices
+for choice in ${choices}
 do
-    case $choice in
+    case ${choice} in
         03) repoPhp ;;
         08) repoPhp ;;
         20) repoPhp ;;
@@ -646,9 +668,9 @@ title "Updating apt";
     notify "The apt package manager is fully updated...";
 breakLine;
 
-for choice in $choices
+for choice in ${choices}
 do
-    case $choice in
+    case ${choice} in
         01) installGit ;;
         02) installNode ;;
         03) installPhp ;;
@@ -656,33 +678,33 @@ do
         05) installPython ;;
         06) installGoLang ;;
         07) installYarn ;;
-        08) 
-            if [ $gotPhp -ne 1 ]; then 
-                installPhp 
+        08)
+            if [[ ${gotPhp} -ne 1 ]]; then
+                installPhp
             fi
             installComposer
         ;;
-        09) 
-            if [ $gotNode -ne 1 ]; then 
-                installNode 
+        09)
+            if [[ ${gotNode} -ne 1 ]]; then
+                installNode
             fi
             installReactNative
         ;;
-        10) 
-            if [ $gotNode -ne 1 ]; then 
-                installNode 
+        10)
+            if [[ ${gotNode} -ne 1 ]]; then
+                installNode
             fi
             installCordova
         ;;
-        11) 
-            if [ $gotNode -ne 1 ]; then 
-                installNode 
+        11)
+            if [[ ${gotNode} -ne 1 ]]; then
+                installNode
             fi
             installPhoneGap
         ;;
-        12) 
-            if [ $gotNode -ne 1 ]; then 
-                installNode 
+        12)
+            if [[ ${gotNode} -ne 1 ]]; then
+                installNode
             fi
             installWebpack
         ;;
@@ -691,15 +713,15 @@ do
         15) installDocker ;;
         16) installKubernetes ;;
         17) installHelm ;;
-        18) 
-            if [ $gotGoLang -ne 1 ]; then 
+        18)
+            if [[ ${gotGoLang} -ne 1 ]]; then
                 installGoLang
             fi
             installSops
         ;;
         19) installPostman ;;
-        20) 
-            if [ $gotPhp -ne 1 ]; then 
+        20)
+            if [[ ${gotPhp} -ne 1 ]]; then
                 installPhp
             fi
             installLaravel
@@ -712,10 +734,16 @@ do
         26) installVsCode ;;
         27) installSublime ;;
         28) installPhpStorm ;;
-        29) installSoftwareCenter ;;   
+        29) installSoftwareCenter ;;
         30) installRemmina ;;
         31) installGoogleSdk ;;
         32) installPopcorn ;;
+        33)
+            if [[ ${gotGoLang} -ne 1 ]]; then
+                installGoLang
+            fi
+            installZsh
+        ;;
     esac
 done
 
