4bd839b4d611091434d4ebdc3998a4c4e697eb9d
Andrew Borg <andrewbrg87@gmail.com>
2018-12-16 01:58:04 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -546,7 +546,7 @@ installZsh() {
 
     sudo apt install -y zsh fonts-powerline;
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
-    curlToFile ${repoUrl}"zsh/.zshrc" "~/.zshrc";
+    curlToFile ${repoUrl}"zsh/.zshrc" ${HOME}"/.zshrc";
     source ~/.zshrc;
 
     installedZsh=1;
