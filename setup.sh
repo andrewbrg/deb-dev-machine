b4926269a7a6ebf646b851f7d810c8a963f07a7a
Andrew Borg <andrewbrg87@gmail.com>
2019-04-09 13:56:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -269,6 +269,7 @@ installPhp() {
 installRuby() {
     title "Installing Ruby with DAPP v${versionDapp}";
     sudo apt install -y ruby-dev gcc pkg-config;
+    sudo gem install mixlib-cli -v 1.7.0;
     sudo gem install dapp -v ${versionDapp};
     breakLine;
 }
