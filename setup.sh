ce2459376308eea77ac2f079661f6cb1802a6379
Andrew Borg <andrewbrg87@gmail.com>
2020-04-19 21:00:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -295,10 +295,11 @@ installRuby() {
 # Python
 ##########################################################
 installPython() {
-    title "Installing Python3 & PIP";
+    title "Installing Python3 with PIP";
     sudo apt install -y build-essential libssl-dev libffi-dev python-dev python3-pip;
     sudo ln -s /usr/bin/pip3 /usr/bin/pip;
-    
+    export PATH=/usr/local/bin/python:$PATH;
+
     installedPython=1;
     breakLine;
 }
